578b71bfe926968da5fe2d993ab84472802d82b5
valkyrie_pilot <valk@randomairborne.dev>
2022-01-07 16:11:14 -0700

    Now theoretically works on windows
    Signed-off-by: valkyrie_pilot <valk@randomairborne.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ mod windows;
 /// Hedgehog's main function
 /// Call this at the very start of your program, will call the entire update sequence (including moving the binary) according to your OS
 /// app_name should be... the name of your app, release_server is a URL to your release server
-/// The release server folder should have the latest version of your app for this OS, configured as so: `<appname>-<osname>` with the `exe` extension for windows.
+/// The release server folder should have the latest version of your app for this OS, configured as so: `<appname>-<osname>-<arch>` with the `exe` extension for windows.
+/// `appname` can be whatever you want, but must remain consistent throughout updates. `osname` should be `bsd`, `linux`, `macOS`, or `windows`, and `arch` should be the Cargo arch
 /// There MUST also be a file with the extension .sha256sum appended onto the filename of the app, so it can detect if it's been updated and validate the download.
 pub fn update(app_name: &str, release_server: &str) {
     #[cfg(target_os = "windows")]
--- src/windows.rs
+++ src/windows.rs
@@ -1,15 +1,62 @@
+use sha2::Digest;
+
 #[cfg(target_os = "windows")]
 pub fn setup(app_name: &str, release_server: &str) {
-    let install_location: &std::path::Path = std::path::Path::new(
-        format!("{}/{}", std::env::var("APPDATA").unwrap(), app_name).as_str(),
-    );
+    let path_string = format!("{}/{}", std::env::var("APPDATA").unwrap(), app_name);
+    let install_location: &std::path::Path = std::path::Path::new(&path_string);
+    let executable_path = std::path::PathBuf::from(format!(
+        "{}/{}{}-windows-{}.exe",
+        path_string,
+        release_server,
+        app_name,
+        std::env::consts::ARCH
+    ));
+    println!("{:?}", install_location);
     if !install_location.exists() {
-        std::fs::create_dir_all(install_location)
+        if let Err(err) = std::fs::create_dir_all(install_location) {
+            panic!("{:?}", err);
+        }
     }
-    std::env::set_current_dir(install_location);
-    let mut reqwest_client_builder = reqwest::Client::builder();
-    reqwest_client_builder.user_agent(format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")));
-    let reqwest_client = reqwest_client_builder.build()?;
-    reqwest_client.get(format!("{}{}-windows.exe.sha256sum", release_server, app_name));
-    return
+    if let Err(err) = std::env::set_current_dir(install_location) {
+        panic!("{:?}", err);
+    }
+    let reqwest_client = reqwest::blocking::Client::builder()
+        .user_agent(format!(
+            "{}/{}",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION")
+        ))
+        .build()
+        .unwrap(); // If the client fails to build something else is very wrong.
+    let request = reqwest_client.get(format!(
+        "{}{}-windows-x86_64.exe.sha256sum",
+        release_server, app_name
+    ));
+    let response = reqwest_client
+        .execute(request.build().unwrap())
+        .unwrap()
+        .text()
+        .unwrap()
+        .trim()
+        .to_string();
+    println!("{:#?}", response);
+    if executable_path.exists() {
+        let mut hasher = sha2::Sha256::new();
+        let mut file = std::fs::File::open(executable_path).unwrap();
+        std::io::copy(&mut file, &mut hasher).unwrap();
+        let hash = hasher
+            .finalize()
+            .into_iter()
+            .map(|x| format!("{:02x}", x))
+            .collect::<String>();
+        println!("Hasher result: `{:#?}`, response: `{}`", hash, response);
+        if hash == response {
+            println!("We already have the latest version, exiting...");
+            return;
+        }
+    } else {
+        println!("{:?}", std::env::current_exe().unwrap());
+        std::fs::rename(std::env::current_exe().unwrap(), executable_path).unwrap();
+        setup(app_name, release_server);
+    }
 }
